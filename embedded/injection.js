e13c1535e4d64170fa995c05443018f71f6581d6
khmy2010 <kehan.see@gmail.com>
2022-12-19 20:07:36 +0800

    include embedded code into CDN.

---
+++ embedded/injection.js
@@ -0,0 +1,40 @@
+(() => {
+  const isLocal = ['localhost', '127.0.0.1'].includes(location.hostname);
+  const isStaging = location.hostname === 'order-history-uat.surge.sh';
+  const apiLink = isLocal ? 'http://localhost:50339' : 'https://api-server-a2d7o3v32a-uc.a.run.app';
+  const appId = (isLocal || isStaging) ? 'a86696f04da5479cceb6afd9081eda2c4d90ba9218e79a25bacfcace75f8fbfe' : 'REPLACE_STORE_ID_HERE';
+
+  const xmlHttpRequest = new XMLHttpRequest();
+  xmlHttpRequest.onreadystatechange = function() {
+    if (xmlHttpRequest.readyState === XMLHttpRequest.DONE && xmlHttpRequest.status === 200) {
+      const responseModel = JSON.parse(xmlHttpRequest.response);
+
+      if (responseModel && responseModel.status === 'SUCCESS' && responseModel.data) {
+        if (!responseModel.data.widgetEnabled) {
+          console.log('Widget is not enabled. Kindly contact developer for support: ', responseModel.data.widgetEnabled);
+          return;
+        }
+
+        const mountingPointId = responseModel.data.mountingPointId;
+
+        const mountingPoint = document.createElement('div');
+        mountingPoint.setAttribute('id', mountingPointId);
+        document.body.appendChild(mountingPoint);
+
+        const version = responseModel.data.version;
+        const injection = document.createElement('script');
+        const cdnBasePath = 'https://cdn.jsdelivr.net/gh/Nimble-Apps-HQ/order-history-public/storefront';
+        injection.src = (isLocal ? version : `${cdnBasePath}/${version}`) + '.js';
+        injection.setAttribute('id', 'nm-order-history-script');
+        injection.dataset.appId = appId;
+        document.body.appendChild(injection);
+      }
+      else {
+        console.log('No-Login Order History PRO experiencing technical issue. Please contact developer for support.');
+      }
+    }
+  }
+
+  xmlHttpRequest.open('GET', apiLink + '/api/storefront/version?appId=' + appId);
+  xmlHttpRequest.send();
+})();
